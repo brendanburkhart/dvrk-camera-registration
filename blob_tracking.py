fc63369389fc4b214608ae2c0818e4b781e3dbae
Brendan Burkhart <brendan.f.burkhart@gmail.com>
2022-08-06 16:58:06 -0400

    Clean up detection registration, measure FPS

--- blob_tracking.py
+++ blob_tracking.py
@@ -18,6 +18,8 @@ import cv2
 import math
 import numpy as np
 import threading
+import time
+import queue
 
 
 # Represents a single tracked detection, with location history information
@@ -48,10 +50,10 @@ class TrackedObject:
         return self.stale
 
     # when a match is found, known position is updated and strength increased by 1,
-    # if no match is found, strength decays by 2 to prevent strength from oscillating
+    # if no match is found, strength decays by drop_off to prevent strength from oscillating
     def update(self, detection):
         if detection is not None:
-            self.position, self.size, self.contour = detection
+            self.position, self.size, self.contour = detection.position, detection.size, detection.contour
             self.location_history.append(self.position)
 
             # cap strength so objects never remain too long
@@ -73,8 +75,8 @@ class ObjectTracking:
         self.max_history = max_history
 
     # mark on object as the primary object to track
-    def set_primary_target(self, position):
-        is_nearby = lambda object: object.distance_to(position) < 1.35 * object.size
+    def set_primary_target(self, position, margin=1.35):
+        is_nearby = lambda object: object.distance_to(position) < margin * object.size
         nearby_objects = [x for x in self.objects if is_nearby(x)]
 
         self.primary_target = nearby_objects[0] if len(nearby_objects) == 1 else None
@@ -89,48 +91,35 @@ class ObjectTracking:
     # register detections from the current frame with tracked objects,
     # removing, updating, and adding tracked objects as needed
     def register(self, detections):
-        # No existing tracked objects, add all detections as new objects
-        if len(self.objects) == 0:
-            for d in detections:
-                self.objects.append(TrackedObject(d, max_history=self.max_history))
+        # create tracked obejcts for detections, mark all detections as unmatched
+        detections = [[TrackedObject(d, max_history=self.max_history) , False] for d in detections]
 
-            return
+        # find closest unmatched detection to object
+        def closest_detection(obj):
+            index, min_distance = None, self.max_distance
 
-        # No detections were found
-        if len(detections) == 0:
-            for obj in self.objects:
+            for i, (detection, matched) in enumerate(detections):
+                distance = obj.distance_to(detection.position)
+                if not matched and distance < min_distance:
+                    index, min_distance = i, distance
+
+            return index
+
+        # update all currently tracked objects
+        for obj in self.objects:
+            index = closest_detection(obj)
+            if index is None:
                 obj.update(None)
-           
-            self.objects = [x for x in self.objects if x.strength > 0]
-            if not self.primary_target in self.objects and self.primary_target is not None:
-                print("Lost track of target! Please click on target to re-acquire")
-                self.primary_target = None
-
-            return
-
-        # Matrix of minimum distance between each detection and each tracked object
-        distances = np.array(
-            [[obj.distance_to(d[0]) for d in detections] for obj in self.objects]
-        )
-
-        # Array of closest tracked object to each detection
-        closest = np.argmin(distances, axis=0)
-        current_object_count = len(self.objects)
-
-        # associate detections with existing tracked objects or add as new objects
-        for i, detection in enumerate(detections):
-            if distances[closest[i], i] <= self.max_distance:
-                self.objects[closest[i]].update(detection)
             else:
-                self.objects.append(TrackedObject(detection, max_history=self.max_history))
+                detections[index][1] = True # mark detection as matched
+                obj.update(detections[index][0])
 
-        # Update tracked objects not associated with current detection
-        closest = np.argmin(distances, axis=1)
-        for j in range(current_object_count):
-            if distances[j, closest[j]] > self.max_distance:
-                self.objects[j].update(None)
+        # detections not matched to existing tracked objects become new tracked objects
+        for detection, matched in detections:
+            if not matched:
+                self.objects.append(detection)
 
-        # Remove stale tracked objects and check if primary target is stale
+        # remove stale tracked objects and check if primary target is stale
         self.objects = [x for x in self.objects if x.strength > 0]
         if not self.primary_target in self.objects and self.primary_target is not None:
             print("Lost track of target! Please click on target to re-acquire")
@@ -145,6 +134,12 @@ class ColorTarget:
         self.contour_min_size = contour_min_size
 
     def find(self, image):
+        def detection(contour):
+            M = cv2.moments(contour)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            size = math.sqrt(M["m00"]/math.pi)
+            return (center, size, contour)
+        
         # Process a frame - find, track, outline all potential targets
         blurred = cv2.medianBlur(image, 2 * self.blur_aperature + 1)
         hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
@@ -154,13 +149,9 @@ class ColorTarget:
             thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
         )
         contours = [c for c in contours if cv2.contourArea(c) > self.contour_min_size]
-
-        moments = [cv2.moments(c) for c in contours]
-        radius = lambda area: math.sqrt(area / math.pi)
-        detections = [
-            ((int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"])), radius(M["m00"]), c)
-            for c, M in zip(contours, moments)
-        ]
+        # De-nest contour points
+        contours = [c.reshape(-1, 2) for c in contours]
+        detections = [detection(c) for c in contours]
 
         return detections
 
@@ -217,7 +208,7 @@ class BlobTracker:
         target_type,
         parameters=Parameters(),
         camera_calibration=None,
-        window_title="CV Calibration",
+        window_title="Vision tracking",
     ):
         self.objects = object_tracker
         self.target_type = target_type
@@ -238,11 +229,14 @@ class BlobTracker:
         cv2.setMouseCallback(self.window_title, self._mouse_callback)
 
     def _init_video(self):
-        self.video_capture = cv2.VideoCapture(0)
+        self.video_capture = cv2.VideoCapture()
         self.video_capture.set(cv2.CAP_PROP_FPS, 30)
+        # auto-focus messes with camera calibration
         self.video_capture.set(cv2.CAP_PROP_AUTOFOCUS, 0)
-        ok = False
-        if self.video_capture.isOpened():
+        # large buffer introduces lag
+        self.video_capture.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+        ok = self.video_capture.open(0)
+        if ok:
             ok, frame = self.video_capture.read()
 
         if not ok:
@@ -262,15 +256,15 @@ class BlobTracker:
         detections = self.target_type.find(image)
         self.objects.register(detections)
 
-        for o in self.objects.objects:
+        for obj in self.objects.objects:
             color = (0, 255, 0)
-            if self.objects.primary_target == o:
+            if self.objects.primary_target == obj:
                 color = (255, 0, 255)
             
-            if o.is_stale():
+            if obj.is_stale():
                 color = (0, 255, 255)
 
-            contour = np.array([(x, y) for [x, y] in o.contour], dtype=np.int)
+            contour = np.array([(x, y) for [x, y] in obj.contour], dtype=np.int)
             cv2.drawContours(image, [contour], -1, color, 3)
 
     def clear_history(self):
@@ -314,6 +308,8 @@ class BlobTracker:
             self._create_window()
             ok = self._init_video()
 
+            times = queue.Queue(maxsize=60)
+
             while ok and not self.should_stop:
                 ok, frame = self.video_capture.read()
                 frame = (
@@ -333,6 +329,10 @@ class BlobTracker:
                 self.draw_axes(frame, self.robot_axes, (255, 255, 0))
                 self.draw_points(frame)
                 cv2.imshow(self.window_title, frame)
+                times.put(time.time())
+                if times.qsize() >= 60:
+                    t = time.time() - times.get()
+                    print("FPS: {}".format(60/t))
                 key = cv2.waitKey(20)
                 key = key & 0xFF  # Upper bits are modifiers (control, alt, etc.)
                 escape = 27
